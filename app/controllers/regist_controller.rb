ae66fc93491ef0e9debfe617943df8c11e5a9127
JironBach <junshimo2@gmail.com>
2019-05-31 03:45:30 +0900

    regist/indexができた

--- app/controllers/regist_controller.rb
+++ app/controllers/regist_controller.rb
@@ -1,2 +1,7 @@
 class RegistController < ApplicationController
+  before_action :sign_in_required, only: [:show]
+  
+  def new
+    render :new
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.htm
-  root 'regist#index'
+  devise_for :users
+  root 'regist#new'
+
 end
